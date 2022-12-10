20f96c742e8cef24613463d260eb7382d26bd024
vardhugopi.k@gmail.com <vardhugopi.k@gmail.com>
2022-12-10 18:45:49 +0000

    subset-dim done

--- models/core/subset_dim_test.sql
+++ models/core/subset_dim_test.sql
@@ -1,24 +1,35 @@
+/* materialized='table') }} */
+-- where trim(VALUE:TYPE,'"') = 'SELLER'
+with subset_test_data as (select * from {{ ref("account_subset_test") }})
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-/*materialized='table') }} */
-
-with subset_test_data as (
-
-    select * from {{ ref('account_subset_test') }} --where trim(VALUE:TYPE,'"') = 'SELLER'
-)
-
-select *
+select
+    id as partner_id,
+    name as name,
+    code as code,
+    type as type,
+    created_by as created_by,
+    updated_by as updated_by,
+    hash(code) as subset_key,
+    current_user() as audit_created_by_user,
+    current_timestamp() as audi_created_dtm,
+    current_user() as audit_modified_by_user,
+    current_timestamp() as audit_modified_dtm,
+    1 as src_sys_key,
+    updated_at,
+    is_disabled,
+    settings,
+    soft_descriptors,
+    counterparty_id,
+    is_dealer,
+    earn_out_minimum_fee,
+    earn_out_plan,
+    enabled_notices,
+    created_at,
+    hash(counterparty_id) as counterparty_key
 from subset_test_data
+/*from subset_delta a
+    left join subset_dim_sf b on hash(code) = b.subset_key
 
-/*
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
--- target/run/learn_dbt/models/core/subset_dim_test.sql
+++ target/run/learn_dbt/models/core/subset_dim_test.sql
@@ -3,16 +3,8 @@
     
 
         create or replace transient table EDW.ANALYTICS.subset_dim_test  as
-        (/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-/*materialized='table') }} */
-
+        (/* materialized='table') }} */
+-- where trim(VALUE:TYPE,'"') = 'SELLER'
 with  __dbt__cte__account_subset_test as (
  
 
@@ -24,19 +16,38 @@ with subset_data as (
 
 select *
 from subset_data
-),subset_test_data as (
+),subset_test_data as (select * from __dbt__cte__account_subset_test)
 
-    select * from __dbt__cte__account_subset_test --where trim(VALUE:TYPE,'"') = 'SELLER'
-)
-
-select *
+select
+    id as partner_id,
+    name as name,
+    code as code,
+    type as type,
+    created_by as created_by,
+    updated_by as updated_by,
+    hash(code) as subset_key,
+    current_user() as audit_created_by_user,
+    current_timestamp() as audi_created_dtm,
+    current_user() as audit_modified_by_user,
+    current_timestamp() as audit_modified_dtm,
+    1 as src_sys_key,
+    updated_at,
+    is_disabled,
+    settings,
+    soft_descriptors,
+    counterparty_id,
+    is_dealer,
+    earn_out_minimum_fee,
+    earn_out_plan,
+    enabled_notices,
+    created_at,
+    hash(counterparty_id) as counterparty_key
 from subset_test_data
+/*from subset_delta a
+    left join subset_dim_sf b on hash(code) = b.subset_key
 
-/*
     Uncomment the line below to remove records with null `id` values
 */
-
--- where id is not null
         );
       
   
