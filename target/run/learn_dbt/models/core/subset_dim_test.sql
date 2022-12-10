beba8ded39b9d94f9266d7b471f19eaa40a4cc52
vardhugopi <vardhugopi.k@gmail.com>
2022-12-10 03:33:20 -0600

    dbt

--- target/run/learn_dbt/models/core/subset_dim_test.sql
+++ target/run/learn_dbt/models/core/subset_dim_test.sql
@@ -1,4 +1,9 @@
-/*
+
+  
+    
+
+        create or replace transient table EDW.ANALYTICS.subset_dim_test  as
+        (/*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
@@ -6,19 +11,32 @@
     Try changing "table" to "view" below
 */
 
+/*materialized='table') }} */
 
+with  __dbt__cte__account_subset_test as (
+ 
 
 with subset_data as (
 
     SELECT trim(VALUE:id,'"') as ID,trim(VALUE:created_at,'"') as CREATED_AT,trim(VALUE:updated_at,'"') as UPDATED_AT,trim(VALUE:created_by,'"') as CREATED_BY,trim(VALUE:updated_by,'"') as UPDATED_BY,trim(VALUE:is_disabled,'"') as IS_DISABLED,trim(VALUE:subset_name,'"') as NAME,trim(VALUE:code,'"') as CODE,trim(VALUE:subset_type,'"') as TYPE,trim(VALUE:counterparty_id,'"') as COUNTERPARTY_ID,trim(VALUE:is_dealer,'"') as IS_DEALER,trim(VALUE:earn_out_minimum_fee,'"') as EARN_OUT_MINIMUM_FEE,trim(VALUE:settings,'"') as SETTINGS,trim(VALUE:soft_descriptors,'"') as SOFT_DESCRIPTORS,trim(VALUE:earn_out_plan,'"') as EARN_OUT_PLAN,VALUE:enabled_notices as ENABLED_NOTICES 
-    FROM ACCOUNT_SUBSET
+    FROM EDW.EXTERNAL.ACCOUNT_SUBSET
 )
 
 select *
 from subset_data
+),subset_test_data as (
+
+    select * from __dbt__cte__account_subset_test --where trim(VALUE:TYPE,'"') = 'SELLER'
+)
+
+select *
+from subset_test_data
 
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
 -- where id is not null
+        );
+      
+  
--- target/run/learn_dbt/models/example/my_first_dbt_model.sql
+++ target/run/learn_dbt/models/example/my_first_dbt_model.sql
@@ -1,4 +1,9 @@
-/*
+
+  
+    
+
+        create or replace transient table EDW.ANALYTICS.my_first_dbt_model  as
+        (/*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
@@ -8,16 +13,22 @@
 
 
 
-with subset_test_data as (
+with source_data as (
 
-    select * from EDW.EXTERNAL.account_subset where trim(VALUE:TYPE,'"') = 'SELLER'
+    select 1 as id
+    union all
+    select null as id
+
 )
 
 select *
-from subset_test_data
+from source_data
 
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
 -- where id is not null
+        );
+      
+  
