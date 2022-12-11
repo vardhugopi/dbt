80aff13cf17cca373402513ee3357fe1d14fa730
vardhugopi.k@gmail.com <vardhugopi.k@gmail.com>
2022-12-11 01:36:24 +0000

    subset_dim

--- target/compiled/learn_dbt/models/core/subset_dim_test.sql
+++ target/compiled/learn_dbt/models/core/subset_dim_test.sql
@@ -11,35 +11,43 @@ with subset_data as (
 
 select *
 from subset_data
-),subset_test_data as (select * from __dbt__cte__account_subset_test)
+),subset_test_data as (select * from __dbt__cte__account_subset_test),
 
-select
-    id as partner_id,
-    name as name,
-    code as code,
-    type as type,
-    created_by as created_by,
-    updated_by as updated_by,
-    hash(code) as subset_key,
-    current_user() as audit_created_by_user,
-    current_timestamp() as audi_created_dtm,
+subset_dim_org as (select * from ANALYTICS.SUBSET_DIM),
+
+final as    (
+    select
+    a.id as partner_id,
+    a.name as name,
+    a.code as code,
+    a.type as type,
+    a.created_by as created_by,
+    a.updated_by as updated_by,
+    hash(a.code) as subset_key,
+    case
+        when b.subset_key is not null
+        then b.audit_created_by_user
+        else current_user()
+    end as audit_created_by_user,
+    case
+        when b.subset_key is not null then b.audit_created_dtm else current_timestamp()
+    end as audit_created_dtm,
     current_user() as audit_modified_by_user,
     current_timestamp() as audit_modified_dtm,
     1 as src_sys_key,
-    updated_at,
-    is_disabled,
-    settings,
-    soft_descriptors,
-    counterparty_id,
-    is_dealer,
-    earn_out_minimum_fee,
-    earn_out_plan,
-    enabled_notices,
-    created_at,
-    hash(counterparty_id) as counterparty_key
-from subset_test_data
-/*from subset_delta a
-    left join subset_dim_sf b on hash(code) = b.subset_key
+    a.updated_at,
+    a.is_disabled,
+    a.settings,
+    a.soft_descriptors,
+    a.counterparty_id,
+    a.is_dealer,
+    a.earn_out_minimum_fee,
+    a.earn_out_plan,
+    a.enabled_notices,
+    a.created_at,
+    hash(a.counterparty_id) as counterparty_key
+    from subset_test_data a
+    left join subset_dim_org b on hash(a.code) = b.subset_key 
+    )
 
-    Uncomment the line below to remove records with null `id` values
-*/
+select * from final
